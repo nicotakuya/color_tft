c5eabb9be7df052ded030b3ee0ec664507914c25
takuya matsubara <takuyamat@gmail.com>
2021-08-15 03:06:13 +0900

    add vector line.

--- color_tft_demo.ino
+++ color_tft_demo.ino
@@ -83,8 +83,7 @@
 #define VRAMHEIGHT       160  // height[pixel]
 #define VRAMSIZE   (VRAMWIDTH*VRAMHEIGHT)
 
-#define ROTATE  0 //TURN SCREEN(0=normal / 1=turn90 / 2=turn180 / 3=turn270)
-
+#define ROTATE  1 //TURN SCREEN(0=normal / 1=turn90 / 2=turn180 / 3=turn270)
 #if ROTATE==0
 #define VRAMXMAX  (VRAMWIDTH-1)
 #define VRAMYMAX  (VRAMHEIGHT-1)
@@ -103,6 +102,9 @@
 #endif
 
 unsigned char vram[VRAMSIZE];
+int def_posx = 0;
+int def_posy = 0;
+unsigned char def_tcolor = 7;
 
 const unsigned char p_gamma_table[] = {
   0x36,0x29,0x12,0x22,0x1C,0x15,0x42,0xB7,0x2F,0x13,0x12,0x0A,0x11,0x0B,0x06
@@ -110,6 +112,112 @@ const unsigned char p_gamma_table[] = {
      
 const unsigned char n_gamma_table[] = {
   0x09,0x16,0x2D,0x0D,0x13,0x15,0x40,0x48,0x53,0x0C,0x1D,0x25,0x2E,0x34,0x39
+};
+
+// sprite pattern
+const unsigned char sp_pattern[] = {
+  0b0011100,
+  0b0111110,
+  0b1110111,
+  0b1100011,
+  0b1110111,
+  0b0111110,
+  0b0011100
+};
+
+// vectol
+const unsigned char vect_table[]={
+    0,/* 0 degrees*/
+    4,/* 1 degrees*/
+    9,/* 2 degrees*/
+   13,/* 3 degrees*/
+   18,/* 4 degrees*/
+   22,/* 5 degrees*/
+   27,/* 6 degrees*/
+   31,/* 7 degrees*/
+   35,/* 8 degrees*/
+   40,/* 9 degrees*/
+   44,/* 10 degrees*/
+   48,/* 11 degrees*/
+   53,/* 12 degrees*/
+   57,/* 13 degrees*/
+   61,/* 14 degrees*/
+   66,/* 15 degrees*/
+   70,/* 16 degrees*/
+   74,/* 17 degrees*/
+   78,/* 18 degrees*/
+   83,/* 19 degrees*/
+   87,/* 20 degrees*/
+   91,/* 21 degrees*/
+   95,/* 22 degrees*/
+   99,/* 23 degrees*/
+  103,/* 24 degrees*/
+  107,/* 25 degrees*/
+  111,/* 26 degrees*/
+  115,/* 27 degrees*/
+  119,/* 28 degrees*/
+  123,/* 29 degrees*/
+  127,/* 30 degrees*/
+  131,/* 31 degrees*/
+  135,/* 32 degrees*/
+  138,/* 33 degrees*/
+  142,/* 34 degrees*/
+  146,/* 35 degrees*/
+  149,/* 36 degrees*/
+  153,/* 37 degrees*/
+  156,/* 38 degrees*/
+  160,/* 39 degrees*/
+  163,/* 40 degrees*/
+  167,/* 41 degrees*/
+  170,/* 42 degrees*/
+  173,/* 43 degrees*/
+  176,/* 44 degrees*/
+  180,/* 45 degrees*/
+  183,/* 46 degrees*/
+  186,/* 47 degrees*/
+  189,/* 48 degrees*/
+  192,/* 49 degrees*/
+  195,/* 50 degrees*/
+  197,/* 51 degrees*/
+  200,/* 52 degrees*/
+  203,/* 53 degrees*/
+  205,/* 54 degrees*/
+  208,/* 55 degrees*/
+  211,/* 56 degrees*/
+  213,/* 57 degrees*/
+  215,/* 58 degrees*/
+  218,/* 59 degrees*/
+  220,/* 60 degrees*/
+  222,/* 61 degrees*/
+  224,/* 62 degrees*/
+  226,/* 63 degrees*/
+  228,/* 64 degrees*/
+  230,/* 65 degrees*/
+  232,/* 66 degrees*/
+  234,/* 67 degrees*/
+  236,/* 68 degrees*/
+  237,/* 69 degrees*/
+  239,/* 70 degrees*/
+  240,/* 71 degrees*/
+  242,/* 72 degrees*/
+  243,/* 73 degrees*/
+  244,/* 74 degrees*/
+  245,/* 75 degrees*/
+  246,/* 76 degrees*/
+  247,/* 77 degrees*/
+  248,/* 78 degrees*/
+  249,/* 79 degrees*/
+  250,/* 80 degrees*/
+  251,/* 81 degrees*/
+  252,/* 82 degrees*/
+  252,/* 83 degrees*/
+  253,/* 84 degrees*/
+  253,/* 85 degrees*/
+  253,/* 86 degrees*/
+  254,/* 87 degrees*/
+  254,/* 88 degrees*/
+  254,/* 89 degrees*/
+  255 /* 90 degrees*/
 };
 
 const unsigned int colortable[]={
@@ -123,17 +231,79 @@ const unsigned int colortable[]={
   0b1111111111000000, /*color 6*/
   0b1111111111111111  /*color 7*/
 };
+typedef struct vecline{
+  int ang;
+  int len;
+} VECLINE,*PVECLINE;
 
-/*    rrrggbbb(3+2+3) */
-//  0b00000000, /*color 0*/
-//  0b00000111, /*color 1*/
-//  0b11100000, /*color 2*/
-//  0b11100111, /*color 3*/
-//  0b00011000, /*color 4*/
-//  0b00011111, /*color 5*/
-//  0b11111000, /*color 6*/
-//  0b11111111  /*color 7*/
+void tft_init(void);
+void tft_from_vram(void);
+void vram_cls(void);
+unsigned char vram_point(int x,int y);
+void vram_pset(int x,int y,unsigned char color);
+void vram_line(int x1 ,int y1 ,int x2 ,int y2 ,char color);
+void vram_fill(int x1 ,int y1 ,int x2 ,int y2 ,char color);
+void vram_locate(int textx, int texty);
+void vram_textcolor(unsigned char color);
+void vram_putch(unsigned char ch);
+void vram_scroll(int xd,int yd);
+void vram_spput(char x,char y, int num,unsigned char color);
+void spi_sendbyte(unsigned char data);
+int vect_x1(int a);
+int vect_y1(int a);
+void vect_put(PVECLINE pV,int vx,int vy,int angle,int zoom,unsigned char color);
 
+// near sin(degree)
+int vect_y1(int a)
+{
+  while(a < 0){
+    a+=360;
+  }
+  while(a >= 360){
+    a-=360;
+  }
+
+  if(a >= 270){              //270～359degrees
+    a-=270;
+    return(-vect_table[a]);
+  }
+  if(a >= 180){              //180～269degrees
+    a-=180;
+    return(vect_table[90-a]);
+  }
+  if(a >= 90){               //90～179degrees
+    a-=90;
+    return(vect_table[a]);
+  }
+  return(-vect_table[90-a]);  //0～90degrees
+}
+
+// near cos(degree)
+int vect_x1(int a)
+{
+  return(vect_y1(a+90));
+}
+
+void vect_put(PVECLINE pV,int vx,int vy,int angle,int zoom,unsigned char color)
+{
+  int i;
+  int tempkak;
+  int x1,y1,x2=0,y2=0;
+
+  for( i=0; pV->ang != -1; i++){
+    tempkak = pV->ang + angle;
+    x1 = vect_x1(tempkak)* pV->len;
+    y1 = vect_y1(tempkak)* pV->len;
+    x1 = (zoom*x1>>10) + vx;
+    y1 = (zoom*y1>>10) + vy;
+    if(i > 0){
+      vram_line(x1,y1,x2,y2,color);
+    }
+    x2 = x1;
+    y2 = y1;
+    pV++;
+  }
+}
 
 void tft_init(void)
 {
@@ -213,8 +383,8 @@ void tft_sendcmd(unsigned char data)
 void tft_sendcmd_15byte(unsigned char cmd,unsigned char *data)
 {
   int cnt;
-  digitalWrite(TFTCS,LOW);
   tft_sendcmd(cmd);
+  digitalWrite(TFTCS,LOW);
   cnt=15;
   while(cnt--){
     spi_sendbyte(*data++);
@@ -254,7 +424,7 @@ void tft_sendcmd_long(unsigned char cmd,unsigned long data)
   delay(1);
 }
 
-// 
+// send VRAM to TFT 
 void tft_from_vram(void)
 {
   int i;
@@ -275,20 +445,24 @@ void tft_from_vram(void)
   delay(1);
 }
 
-
 void vram_cls(void)
 {
   int i;
+  unsigned char *ptr;
 
-  for(i=0; i<VRAMSIZE; i++){
-    vram[i] = 0;
+  ptr = (unsigned char *)vram;
+  i = VRAMSIZE;
+  while(i--){
+    *ptr = 0;
+    ptr++;
   }
 }
 
-//ピクセル取得(X座標,Y座標)
+//get pixel(X座標,Y座標)
 unsigned char vram_point(int x,int y)
 {
   int i;
+  unsigned int color;
 
 #if ROTATE==1
   i=x;
@@ -314,8 +488,7 @@ unsigned char vram_point(int x,int y)
   return(vram[i]);
 }
 
-
-//ピクセル描画(X座標,Y座標,カラーコード)
+//draw pixel(X座標,Y座標,カラーコード)
 void vram_pset(int x,int y,unsigned char color)
 {
   int i;
@@ -344,8 +517,19 @@ void vram_pset(int x,int y,unsigned char color)
   vram[i] = color;
 }
 
+//box fill(X1,Y1,X2,Y2,color)
+void vram_fill(int x1 ,int y1 ,int x2 ,int y2 ,char color)
+{
+  int x,y;
 
-//ライン描画(X1座標,Y1座標,X2座標,Y2座標,カラーコード)
+  for(y=y1; y<=y2; y++){
+    for(x=x1; x<=x2; x++){
+      vram_pset(x, y ,color); //ドット描画
+    }
+  }
+}
+
+//draw line(X1,Y1,X2,Y2,color)
 void vram_line(int x1 ,int y1 ,int x2 ,int y2 ,char color)
 {
   int xd;    // X2-X1座標の距離
@@ -389,7 +573,144 @@ void vram_line(int x1 ,int y1 ,int x2 ,int y2 ,char color)
   }
 }
 
+//locate(x,y)
+void vram_locate(int textx, int texty)
+{
+  def_posx = textx;
+  def_posy = texty;
+}
 
+void vram_textcolor(unsigned char color)
+{
+  def_tcolor = color;
+}
+
+//print character(x,y,character,color)
+void vram_putch(unsigned char ch)
+{
+  char i,j;
+  unsigned char bitdata;
+  int p;
+
+  if(ch < 0x20)return;
+  if(def_posx > VRAMXMAX){
+    def_posx = 0;
+    def_posy += 8;
+    if(def_posy > VRAMYMAX){
+      def_posy = VRAMYMAX-7;    
+      vram_scroll(0,8);
+    }
+  }  
+  
+  p = ((int)(ch - 0x20) * 8);
+  for(i=0 ;i<8 ;i++) {
+    bitdata = font[p++];
+    for(j=0; j<8; j++){
+      if(bitdata & 0x80){
+        vram_pset(def_posx+j,def_posy+i,def_tcolor);
+      }
+      bitdata <<= 1;
+    }
+  }
+  def_posx += 8;
+}
+
+// put string
+void vram_putstr(unsigned char *p)
+{
+  while(*p != 0){
+    vram_putch( *p++ );
+  }
+}
+
+// print decimal number
+void vram_putnum(int num)
+{
+  vram_putch( '0'+(num / 10));
+  vram_putch( '0'+(num % 10));
+}
+
+void vram_putdec(unsigned int x)
+{
+    unsigned char ch;
+    unsigned int shift=10000;
+  
+  while(shift > 0){
+    ch = (x / shift) % 10;
+    ch += '0';
+    vram_putch(ch);
+    shift /= 10;
+  }
+}
+// print Hexadecimal number
+void vram_puthex(unsigned char num)
+{
+  unsigned char ch;
+  char shift=4;
+
+  while(shift >= 0){
+    ch = (num >> shift) & 0xf;
+    if(ch < 10){
+      ch += '0';
+    }else{
+      ch += ('A'-10);
+    }
+    vram_putch( ch);
+    shift -= 4;
+  }
+}
+
+#define VRSPSIZE 7
+// print sprite
+void vram_spput(char x,char y, int num,unsigned char color)
+{
+  char i,j;
+  unsigned char dat;
+
+  num *= VRSPSIZE;
+  x -= (VRSPSIZE/2);
+  y -= (VRSPSIZE/2);
+
+  for(j=0; j<VRSPSIZE; j++){
+    dat = sp_pattern[num++];
+    for(i=0; i<VRSPSIZE; i++){
+      if(dat & (1<<(VRSPSIZE-1))){
+        vram_pset(x+i,y+j,color);
+      }
+      dat <<= 1;
+    }
+  }
+}
+
+void vram_spclr(char x,char y)
+{
+  char i,j;
+
+  x -= (VRSPSIZE/2);
+  y -= (VRSPSIZE/2);
+
+  for(j=0; j<VRSPSIZE; j++){
+    for(i=0; i<VRSPSIZE; i++){
+      vram_pset(x+i,y+j,0);
+    }
+  }
+}
+
+//scroll(delta x,delta y)
+void vram_scroll(int xd,int yd)
+{
+  int x,y;
+  unsigned char color;
+
+  for(y=0;y<(VRAMYMAX+1);y++){
+    for(x=0;x<(VRAMXMAX+1);x++){
+      color = vram_point(x+xd, y+yd);
+      vram_pset(x,y,color);
+    }
+  }
+}
+
+//----
 void setup(void)
 {
   tft_init();
@@ -399,81 +720,38 @@ void setup(void)
 //  }
 }
 
-
-// 1キャラクタをVRAM転送(x,y,chキャラクターコード,color)
-void vram_putch(int textx, int texty, unsigned char ch,unsigned char color)
-{
-  char i,j;
-  unsigned char bitdata;
-  int p;
-
-  if(ch < 0x20)return;
-  p = ((int)(ch - 0x20) * 8);
-  for(i=0 ;i<8 ;i++) {
-    bitdata = font[p++];
-    for(j=0; j<8; j++){
-      if(bitdata & 0x80)vram_pset(textx+j,texty+i,color);
-      bitdata <<= 1;
-    }
-  }
-}
-
-void vram_scroll(int xd,int yd){
-  int x,y;
-  int xn,yn;
-  unsigned char color;
-
-  for(y=0;y<(VRAMYMAX+1);y++){
-    for(x=0;x<(VRAMXMAX+1);x++){
-      xn = x+xd;
-      yn = y+yd;
-      if((xn > VRAMXMAX)||(yn > VRAMYMAX)){
-        color = 0;
-      }else{
-        color = vram_point(xn, yn);
-      }
-      vram_pset(x,y,color);
-    }
-  }
-}
-
-
-void chartest(void)
+void chardemo(void)
 {
   int x,y;
   unsigned char chrnum;
   unsigned char color;
   int timeout;
 
-  timeout=1000;
-  x=0;
-  y=VRAMYMAX-7;
   chrnum=0x20;
-  color=7;
+  vram_locate(0, VRAMYMAX-7);
+  color = 7;
+  vram_textcolor(color);
+  timeout=300;
   while(timeout--){
-    vram_putch(x,y,chrnum,color);
+    vram_putch(chrnum);
     chrnum++;
     if(chrnum > 0x7f){
       chrnum=0x20;
       color++;
       if(color > 7)color=1;
+      vram_textcolor(color);
     }
-    x = (x+8) % (VRAMXMAX+1);
-    if(x == 0){
-      vram_scroll(0,8);
-    }  
     tft_from_vram();
   }
 }
   
-void bound(void)
+void linedemo(void)
 {
   int xa,ya,xb,yb;
   int xa1,ya1,xb1,yb1;
   unsigned char color;
   int timeout;
 
-  timeout = 1000;
   xa =random(VRAMXMAX-1)+1;
   ya =random(VRAMYMAX-1)+1;
   xb =random(VRAMXMAX-1)+1;
@@ -484,6 +762,7 @@ void bound(void)
   yb1 =random(5)-2;
 
   color=0;
+  timeout = 300;
   while(timeout--){
     xa += xa1;
     ya += ya1;
@@ -499,8 +778,206 @@ void bound(void)
   }
 }
 
+int fnc_abs(int a)
+{
+  if(a<0)a = -a;
+  return (a);
+}
+
+int fnc_sgn(int a)
+{
+  if(a<0)return(-1);
+  return (1);
+}
+
+void balldemo(void)
+{
+  typedef struct ball{
+    int x;
+    int y;
+    int x1;
+    int y1;
+  } ST_BALL;   // 構造体
+
+  #define BALLMAX 14
+  ST_BALL ball[BALLMAX];
+
+  #define MLT 3
+
+  int x2,y2;
+  int xd,yd;
+  unsigned char i,j;
+  int timeout;
+
+  for(i=0;i<BALLMAX;i++){
+    ball[i].x = random(VRAMXMAX)*MLT;
+    ball[i].y = random(VRAMYMAX)*MLT;
+    ball[i].x1 = random(MLT*2)-MLT;
+    ball[i].y1 = random(MLT*2)-MLT;
+  }
+
+  timeout=300;
+  while(timeout--){
+    vram_cls();
+    
+    for(i=0; i<BALLMAX; i++){
+      x2 = ball[i].x;
+      y2 = ball[i].y;
+
+      x2 += ball[i].x1;
+      y2 += ball[i].y1;
+      for(j=0 ;j<BALLMAX ;j++){
+        if(i==j)continue;
+        xd = x2-ball[j].x;
+        yd = y2-ball[j].y;
+
+        if( (fnc_abs(xd) < 7*MLT)&&(fnc_abs(yd) < 7*MLT)){
+          ball[i].x1 = fnc_sgn(xd)*(random(MLT*2)+1);
+          ball[i].y1 = fnc_sgn(yd)*(random(MLT*2)+1);
+          ball[j].x1 = -ball[i].x1;
+          ball[j].y1 = -ball[i].y1;
+        }
+      }
+      ball[i].x = x2;
+      ball[i].y = y2;
+
+      x2/=MLT;
+      y2/=MLT;
+      if(x2 <= 3) ball[i].x1 = (random(MLT*2)+1);
+      if(x2 >= (VRAMXMAX-3)) ball[i].x1 = -(random(MLT*2)+1);
+      if(y2 <= 3) ball[i].y1 = (random(MLT*2)+1);
+      if(y2 >= (VRAMYMAX-3)) ball[i].y1 = -(random(MLT*2)+1);
+      
+      vram_spput(x2,y2,0,(i % 7)+1);
+    }
+    tft_from_vram();
+  }
+}
+
+void vectoldemo(void)
+{
+  typedef struct inseki{
+    int x;
+    int y;
+    int x1;
+    int y1;
+    int ang;
+    int a1;
+    int zoom;
+  } INSEKI, *PINSEKI;
+  #define OFST 5
+  #define INSEKIMAX 10
+  INSEKI insk[INSEKIMAX];
+  PINSEKI pInsk;
+  int i;
+  int timeout;
+  int tx,ty;
+  VECLINE myship[5] = {
+    {  40 ,6},  /*  */
+    {  320,6},  /*  */
+    {  180,6},  /*  */
+    {  40 ,6},  /*  */
+    {  -1 ,-1}  /*  */
+  };
+
+  for(i=0; i<INSEKIMAX; i++){
+    insk[i].x=-999;
+    insk[i].y=0;
+    insk[i].x1=0;
+    insk[i].y1=0;
+  }
+
+  timeout=300;
+  while(timeout--){
+    vram_cls();   // clear VRAM
+
+    pInsk = &insk[0];
+    for(i=0; i<INSEKIMAX; i++,pInsk++){
+      tx = pInsk->x;
+      ty = pInsk->y;
+      tx += pInsk->x1;
+      ty += pInsk->y1;
+      if((ty< -(8<<OFST))||(ty>((VRAMYMAX+8)<<OFST))||(tx < -(8<<OFST))||(tx>((VRAMXMAX+8)<<OFST))){
+        if(random(2)){
+          pInsk->x = -8<<OFST;
+          pInsk->x1 = random(20)+30;
+        }else{
+          pInsk->x = (VRAMXMAX+8)<<OFST;
+          pInsk->x1 = -random(20)-30;
+        }
+        pInsk->ang = random(36)*10;
+        pInsk->y = random(6)*((VRAMYMAX<<OFST)/6);
+        pInsk->y1 = random(13)-7;
+        pInsk->a1 = random(13)-6;
+        pInsk->zoom = random(8)+4;
+        continue;
+      }
+      vect_put((PVECLINE)myship, tx>>OFST, ty>>OFST, pInsk->ang, pInsk->zoom,(i%7)+1);
+      pInsk->x = tx;
+      pInsk->y = ty;
+      pInsk->ang += pInsk->a1;
+      if(pInsk->ang >=360) pInsk->ang -= 360;
+      if(pInsk->ang <   0) pInsk->ang += 360;
+    }
+    tft_from_vram();
+  }
+}
+
+//life game
+void lifegame(void)
+{
+  #define WSIZE 60
+
+  int x,y;
+  int ax,bx;
+  unsigned char color;
+  char touch;
+  int i;
+  int timeout;
+  int lx1[8]={ 0, 1, 1, 1, 0,-1,-1,-1};
+  int ly1[8]={-1,-1, 0, 1, 1, 1, 0,-1};
+
+  vram_cls();
+  for(i=0; i<500; i++){
+    x= random(WSIZE);
+    y= random(WSIZE);
+    vram_pset(x,y,7);
+  }
+
+  ax=0;
+  bx=80;
+  timeout=300;
+  while(timeout--){
+    for(y=0; y<WSIZE; y++){
+      for( x=0; x<WSIZE; x++){
+        color=0;
+        touch=0;
+        for( i=0;i<8;i++){
+          if(vram_point(ax+x+lx1[i],y+ly1[i])){
+            touch++;
+            if(touch>=4){
+              break;
+            }
+          }
+        }
+        if(touch==2) color=vram_point(ax+x,y);
+        if(touch==3) color=7;
+        vram_pset(bx+x,y,color);
+      }
+    }
+    x=ax; //swap ax,bx
+    ax=bx;
+    bx=x;
+
+    tft_from_vram();
+  }
+}
+
 void loop()
 {
-  chartest();
-  bound();
+  vectoldemo();
+  lifegame();
+  balldemo();
+  chardemo();
+  linedemo();
 }
